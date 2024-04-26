57643b104dabd9667bc3f7d06656b2f7277f29bf
Katri <katri.laamala@outlook.com>
2024-04-26 14:18:43 +0300

    Exercise 12.7: Little bit of MongoDB coding

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,14 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo); 
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const { text, done } = req.body
+  const todo = await Todo.findOneAndUpdate({ text: req.todo.text }, { text: text, done: done})
+  res.send(todo)
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
