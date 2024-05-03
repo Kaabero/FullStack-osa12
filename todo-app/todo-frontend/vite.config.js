be3e0b1b07c1220d0906c443c0e48c8b9bb7d080
Katri <katri.laamala@outlook.com>
2024-05-03 14:20:07 +0300

    Exercise 12.15: Set up a frontend development environment

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true
-  },
   server: {
     watch: {
       usePolling: true
     }
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true
   }
+  
 })
